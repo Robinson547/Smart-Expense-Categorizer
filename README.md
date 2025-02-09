00cb40309374a32131e51cd23ae3c130bfe9e551
Robinson547 <santiagocompani88@gmail.com>
2025-02-09 22:22:00 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Smart-Expense-Categorizer
+# Smart Expense Categorizer
+
+This is a Python project for smart expense categorizer. More details will be added soon.
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Smart Expense Categorizer
+This is the main script for the smart expense categorizer project.
+"""
+
+def main():
+    print("Welcome to Smart Expense Categorizer!")
+
+if __name__ == "__main__":
+    main()
